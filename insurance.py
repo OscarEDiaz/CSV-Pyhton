2a9e92beed1b587d2335cb8adfce940ece92df37
Oscar Ramirez <A01660338@itesm.mx>
2021-05-04 18:47:46 -0500

    Modified insurance.py

--- insurance.py
+++ insurance.py
@@ -1,10 +1,10 @@
-import csv
+import csv # Libreria para leer archivos de CSV /Comma Separated Values/
 with open('insurance.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     contador_male = 0
     contador_female = 0
-    for row in spamreader:
-        newList = (row[0].split(","))
+    for row in reader: # row va tomando un valor de lista por cada que avanza en reader, el cual es una lista grande de listas.
+        newList = (row[0].split(",")) # Dado que row es solo una lista con un solo string, solo tiene posicion 0
         if newList[1] == "female":
             contador_female += 1
         elif newList[1] == "male":
